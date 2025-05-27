96c08be213cfcac2796c53c26f511007e92e8dd7
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2025-05-27 22:03:32 +0100

    Remove build files

--- build/src/utils/roles.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserRole = void 0;
-var UserRole;
-(function (UserRole) {
-    UserRole["User"] = "user";
-    UserRole["Admin"] = "admin";
-    UserRole["Moderator"] = "moderator";
-})(UserRole || (exports.UserRole = UserRole = {}));
