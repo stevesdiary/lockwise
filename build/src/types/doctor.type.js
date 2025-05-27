b05e2d2caec2947e497d1f15d7105493627a1f97
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-05-27 22:05:32 +0100

    Stop tracking build files

--- build/src/types/doctor.type.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DoctorSpecialty = void 0;
-var DoctorSpecialty;
-(function (DoctorSpecialty) {
-    DoctorSpecialty["CARDIOLOGY"] = "CARDIOLOGY";
-    DoctorSpecialty["DERMATOLOGY"] = "DERMATOLOGY";
-    DoctorSpecialty["PEDIATRICS"] = "PEDIATRICS";
-})(DoctorSpecialty || (exports.DoctorSpecialty = DoctorSpecialty = {}));
