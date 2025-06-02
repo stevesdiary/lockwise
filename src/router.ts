69a3582350400a404622e553871da4a157fd7803
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2025-06-02 15:30:49 +0100

    feat: add permission router to handle permission-related routes

--- src/router.ts
+++ src/router.ts
@@ -4,7 +4,8 @@ import userRouter from "./modules/user/user.route";
 import loginRouter from "./modules/user/login.route";
 import paymentRouter from "./modules/payment/payment.route";
 import estateRouter from "./modules/estate/estate.route";
-import roleRouter from "./modules/role/role.route";
+import roleRouter from "./modules/role/role.router";
+import permissionRouter from "./modules/permission/permission.route"
 
 const router = Router();
 
@@ -13,6 +14,8 @@ router.use("/log", loginRouter);
 router.use('/payment', paymentRouter);
 router.use('/estate', estateRouter);
 router.use('/role', roleRouter);
+router.use('/permission', permissionRouter);
+
 
 
 
