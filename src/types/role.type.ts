098732c94a1198d9fb357e15184d83b6b60f1cf2
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2025-06-02 17:07:14 +0100

    feat: add role type definitions for creation, update, response, and list attributes

---
+++ src/types/role.type.ts
@@ -0,0 +1,16 @@
+import { CreationAttributes } from "sequelize";
+import { Role } from "../modules/role/role.model";
+
+export type RoleCreationAttributes = CreationAttributes<Role>;
+
+export type RoleUpdateAttributes = Partial<Omit<RoleCreationAttributes, "id">>;
+
+export type RoleResponse = Omit<Role, "createdAt" | "updatedAt"> & {
+  permissions?: string[];
+};
+export type RoleListResponse = {
+  roles: RoleResponse[];
+  total: number;
+  page: number;
+  limit: number;
+};
