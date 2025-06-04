9d2083601c64e9bff6dbd01e1523a00d98d99980
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2025-06-04 07:46:58 +0100

    refactor: comment out payment router import and usage

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,7 @@ import express, { Router } from "express";
 
 import userRouter from "./modules/user/user.route";
 import loginRouter from "./modules/user/login.route";
-import paymentRouter from "./modules/payment/payment.route";
+// import paymentRouter from "./modules/payment/payment.route";
 import estateRouter from "./modules/estate/estate.route";
 import roleRouter from "./modules/role/role.router";
 import permissionRouter from "./modules/permission/permission.route"
@@ -11,7 +11,7 @@ const router = Router();
 
 router.use("/user", userRouter);
 router.use("/log", loginRouter);
-router.use('/payment', paymentRouter);
+// router.use('/payment', paymentRouter);
 router.use('/estate', estateRouter);
 router.use('/role', roleRouter);
 router.use('/permission', permissionRouter);
