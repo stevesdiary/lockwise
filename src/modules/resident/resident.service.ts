7e0760cf7d791792eb2b0efc3046577f0d844bf0
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2025-06-04 07:55:51 +0100

    refactor: comment out ResidentService methods and imports for future implementation

--- src/modules/resident/resident.service.ts
+++ src/modules/resident/resident.service.ts
@@ -1,32 +1,32 @@
-import { ResidentRepository } from '../repositories/resident.repository';
-import { Resident } from './resident.model';
+// import { ResidentRepository } from '../repositories/resident.repository';
+// import { Resident } from './resident.model';
 
-export class ResidentService {
-  private readonly residentRepo = new ResidentRepository();
+// export class ResidentService {
+//   private readonly residentRepo = new ResidentRepository();
   
-  async getAllResidents(): Promise<Resident[]> {
-    return await this.residentRepo.findAll();
-  }
+//   async getAllResidents(): Promise<Resident[]> {
+//     return await this.residentRepo.findAll();
+//   }
   
-  async getOneResident(residentId: string, estateId: string): Promise<Resident | null> {
-    return await this.residentRepo.findById(residentId);
-  }
+//   async getOneResident(residentId: string, estateId: string): Promise<Resident | null> {
+//     return await this.residentRepo.findById(residentId);
+//   }
 
-  async createResident(residentData: Partial<Resident>): Promise<Resident> {
-    return await this.residentRepo.create(residentData);
-  }
+//   async createResident(residentData: Partial<Resident>): Promise<Resident> {
+//     return await this.residentRepo.create(residentData);
+//   }
 
-  async updateResident(residentId: string, data: Partial<Resident>): Promise<Resident | null> {
-    return await this.residentRepo.update(residentId, data);
-  }
+//   async updateResident(residentId: string, data: Partial<Resident>): Promise<Resident | null> {
+//     return await this.residentRepo.update(residentId, data);
+//   }
 
-  async deleteResident(residentId: string): Promise<boolean> {
-    return await this.residentRepo.delete(residentId);
-  }
+//   async deleteResident(residentId: string): Promise<boolean> {
+//     return await this.residentRepo.delete(residentId);
+//   }
 
-  async getResidentsByEstate(estateId: string): Promise<Resident[]> {
-    return await this.residentRepo.findAllByEstate(estateId);
-  }
-}
+//   async getResidentsByEstate(estateId: string): Promise<Resident[]> {
+//     return await this.residentRepo.findAllByEstate(estateId);
+//   }
+// }
 
-export default ResidentService;
+// export default ResidentService;
