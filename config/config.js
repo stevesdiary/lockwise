0de83eb892270efc0bdc465b64661b8b62b46b08
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-09-27 15:54:18 +0100

    Add docker files

--- config/config.js
+++ config/config.js
@@ -1,28 +1,53 @@
 const dotenv = require('dotenv');
-const fs = require('fs');
 
 dotenv.config();
 
-const databaseConnection = {
+const baseConfig = {
   dialect: 'postgres',
   seedersStorage: 'sequelize',
   seedersStorageTableName: 'seeders',
   migrationStorageTableName: 'migrations',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: true
-    }
-  }
-}
+};
 
 module.exports = {
-  test: databaseConnection,
-  development: databaseConnection,
-  production: databaseConnection
+  development: {
+    ...baseConfig,
+    host: process.env.DEV_DB_HOST || 'localhost',
+    username: process.env.DEV_DB_USER || 'postgres',
+    password: process.env.DEV_DB_PASSWORD,
+    database: process.env.DEV_DB_NAME,
+    port: process.env.DEV_DB_PORT || 5432,
+    dialectOptions: {
+      ssl: {
+        require: process.env.SSL,
+        rejectUnauthorized: false
+      }
+    }
+  },
+  
+  production: {
+    ...baseConfig,
+    host: process.env.PROD_DB_HOST,
+    username: process.env.PROD_DB_USER,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    port: process.env.PROD_DB_PORT || 5432,
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  },
+  
+  test: {
+    ...baseConfig,
+    host: process.env.TEST_DB_HOST || 'localhost',
+    username: process.env.TEST_DB_USER || 'postgres',
+    password: process.env.TEST_DB_PASSWORD,
+    database: process.env.TEST_DB_NAME || 'lockwise_test',
+    port: process.env.TEST_DB_PORT || 5432,
+    logging: false
+  }
 }
