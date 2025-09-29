e54de4fd36b0df3d48d38d4fbb9c77547f135329
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-09-29 12:40:41 +0100

    Update user, estate, role and login services to use ddirect model for db query

--- src/services/estate.service.ts
+++ src/services/estate.service.ts
@@ -9,90 +9,110 @@ class EstateService {
     this.estateRepository = new EstateRepository();
   }
   async createEstate(data: EstateCreationAttributes): Promise<ApiResponse> {
-    const estate = await this.estateRepository.create(data);
-    if (!estate) {
-      throw new Error('Failed to create estate');
+    try {
+      const estate = await this.estateRepository.create(data);
+      if (!estate) {
+        throw new Error('Failed to create estate');
+      }
+      return {
+        statusCode: 201,
+        status: 'success',
+        message: 'Estate created successfully',
+        data: estate
+      };
+    } catch (error) {
+      throw error;
     }
-    return {
-      statusCode: 201,
-      status: 'success',
-      message: 'Estate created successfully',
-      data: estate
-    };
   }
 
   async getAllEstates(): Promise<ApiResponse> {
-    const estate = await this.estateRepository.findAll();
-    if (!estate || estate.length === 0) {
-      return {
-        statusCode: 404,
-        status: 'fail',
-        message: 'No estates found',
-        data: []
+    try {
+      const estate = await this.estateRepository.findAll();
+      if (!estate || estate.length === 0) {
+        return {
+          statusCode: 404,
+          status: 'fail',
+          message: 'No estates found',
+          data: []
+        }
       }
-    }
-    return {
-      statusCode: 200,
-      status: 'success',
-      message: 'Estates retrieved successfully',
-      data: estate
+      return {
+        statusCode: 200,
+        status: 'success',
+        message: 'Estates retrieved successfully',
+        data: estate
+      }
+    } catch (error) {
+      throw error;
     }
   }
 
   async getOneEstate(estate_id: string, estate_code: string): Promise<ApiResponse | null> {
-    if (!estate_id || !estate_code) {
+    try {
+      if (!estate_id || !estate_code) {
+        return {
+          statusCode: 400,
+          status: 'fail',
+          message: 'Estate ID or Code is required',
+          data: null
+        };
+      }
+      const estate = await this.estateRepository.findById(estate_id);
+      if (!estate) {
+        return {
+          statusCode: 404,
+          status: 'fail',
+          message: 'Estate not found',
+          data: null
+        };
+      }
       return {
-        statusCode: 400,
-        status: 'fail',
-        message: 'Estate ID or Code is required',
-        data: null
-      };
-    }
-    const estate = await this.estateRepository.findById(estate_id);
-    if (!estate) {
-      return {
-        statusCode: 404,
-        status: 'fail',
-        message: 'Estate not found',
-        data: null
-      };
-    }
-    return {
-      statusCode: 200,
-      status: 'success',
-      message: 'Estates retrieved successfully',
-      data: estate
+        statusCode: 200,
+        status: 'success',
+        message: 'Estates retrieved successfully',
+        data: estate
+      }
+    } catch (error) {
+      throw error;
     }
   }
 
   async updateEstate(id: string, data: Partial<Estate>): Promise<ApiResponse | null> {
-    const estate = await this.estateRepository.update(id, data);
-    if (!estate) {
-      throw new Error('No estates found');
-    }
-    return {
-      statusCode: 200,
-      status: 'success',
-      message: 'Estates retrieved successfully',
-      data: estate
+    try {
+      const estate = await this.estateRepository.update(id, data);
+      if (!estate) {
+        throw new Error('No estates found');
+      }
+      return {
+        statusCode: 200,
+        status: 'success',
+        message: 'Estates retrieved successfully',
+        data: estate
+      }
+    } catch (error) {
+      throw error;
     }
   }
 
   async deleteEstate(estate_id: string): Promise<ApiResponse> {
-    const deleteEstate = await this.estateRepository.delete(estate_id);
-    if (!deleteEstate) {
+    try {
+      const deleteEstate = await this.estateRepository.delete(estate_id);
+      if (!deleteEstate) {
+        return {
+          statusCode: 404,
+          status: 'fail',
+          message: 'Estate not found or already deleted',
+          data: null
+        }
+      }
       return {
-        statusCode: 404,
-        status: 'fail',
-        message: 'Estate not found or already deleted',
+        statusCode: 200,
+        status: 'success',
+        message: 'Estate record deleted successfully',
         data: null
       }
-    }
-    return {
-      statusCode: 200,
-      status: 'success',
-      message: 'Estate record deleted successfully',
-      data: null
+    } catch (error) {
+      throw error;
     }
   }
 }
