760053a749f4ee438661d81ecfa55f8a620c89d3
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-10-03 17:18:17 +0100

    feat(utils): add access code generation functions
    - Add async generateAccessCode method using AccessCodeService
    - Add generateCustomAccessCode method for custom codes by event name
    - Export new access code functions alongside existing ID generators

--- src/utils/idGenerator.ts
+++ src/utils/idGenerator.ts
@@ -1,5 +1,5 @@
 import {customAlphabet} from 'nanoid';
-// import * as nanoid from 'nanoid';
+import AccessCodeService from '../services/access.code.service';
 
 const nanoid = customAlphabet("1234567890", 6)();
 
@@ -15,9 +15,19 @@ function generateId(): string {
   return Math.floor(10000000 + Math.random() * 90000000).toString(); // Generates an 8-digit random number
 }
 
+async function generateAccessCode(estateId: string): Promise<string> {
+  return await AccessCodeService.generateCode(estateId);
+}
+
+function generateCustomAccessCode(eventName: string): string {
+  return AccessCodeService.generateCustomCode(eventName);
+}
+
 export default { 
   generateEstateId, 
   generateVerificationCode, 
   generateId, 
+  generateAccessCode,
+  generateCustomAccessCode,
   nanoid  
 };
